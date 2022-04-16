5bbbecd08b8fcaf658f741b4c4193f1c0e9b6884
Archontis <77233507+ArchontisKostis@users.noreply.github.com>
2022-04-16 15:18:39 +0300

    New Branch

---
+++ photo.js
@@ -0,0 +1,6 @@
+const screenshotTarget = document.body;
+
+html2canvas(screenshotTarget).then((canvas) => {
+    const base64image = canvas.toDataURL("image/png");
+    window.location.href = base64image;
+});
